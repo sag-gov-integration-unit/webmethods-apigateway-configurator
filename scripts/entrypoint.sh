216d8bd4e3d5539703745fe9c4f64f1ddb2288a0
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2022-07-22 20:52:23 -0400

    updates with more playbooks

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -35,7 +35,12 @@ if [ "${apigw_settings_lburls_configure}" == "true" ]; then
     exec_ansible_playbook config_lburls.yaml
 fi
 
-## config_keystores / truststore
+## promotion_stages
+if [ "${apigw_data_promotions_stages_configure}" == "true" ]; then
+    exec_ansible_playbook config_promotion_stages.yaml
+fi
+
+## config_keystores / truststores
 if [ "${apigw_settings_keystore_configure}" == "true" ]; then
     exec_ansible_playbook config_keystores.yaml
 fi
@@ -44,18 +49,9 @@ if [ "${apigw_settings_truststore_configure}" == "true" ]; then
     exec_ansible_playbook config_truststores.yaml
 fi
 
-## config_ssl
+## config_ssl inbound / outbound messages
 if [ "${apigw_settings_ssl_inbound_outbound_configure}" == "true" ]; then
     exec_ansible_playbook config_ssl_inout_connections.yaml
-fi
-
-if [ "${apigw_settings_ssl_runtimeport_configure}" == "true" ]; then
-    exec_ansible_playbook config_ssl_port.yaml
-fi
-
-## config_saml
-if [ "${apigw_settings_saml_configure}" == "true" ]; then
-    exec_ansible_playbook config_saml.yaml
 fi
 
 ## config portal gateways
@@ -63,10 +59,66 @@ if [ "${apigw_settings_portalgateway_configure}" == "true" ]; then
     exec_ansible_playbook config_portalgateway.yaml
 fi
 
+## config_ldap
+if [ "${apigw_settings_ldap_configure}" == "true" ]; then
+    exec_ansible_playbook config_ldap.yaml
+fi
+
+## config_saml
+if [ "${apigw_settings_saml_configure}" == "true" ]; then
+    exec_ansible_playbook config_saml.yaml
+fi
+
+## config ports
+if [ "${apigw_settings_ports_configure}" == "true" ]; then
+    exec_ansible_playbook config_ports.yaml
+fi
+
 ## import archives
 if [ "${apigw_data_archives_import}" == "true" ]; then
     exec_ansible_playbook import_archives.yaml
 fi
+
+## aliases
+if [ "${apigw_data_aliases_configure}" == "true" ]; then
+    exec_ansible_playbook config_aliases.yaml
+fi
+
+## plans
+if [ "${apigw_data_plans_configure}" == "true" ]; then
+    exec_ansible_playbook config_plans.yaml
+fi
+
+## packages
+if [ "${apigw_data_packages_configure}" == "true" ]; then
+    exec_ansible_playbook config_packages.yaml
+fi
+
+## applications
+if [ "${apigw_data_applications_configure}" == "true" ]; then
+    exec_ansible_playbook config_applications.yaml
+fi
+
+## activate/deactivate packages
+if [ "${apigw_data_packages_changestate}" == "true" ]; then
+    exec_ansible_playbook activate_packages.yaml
+fi
+
+## activate/deactivate apis
+if [ "${apigw_data_apis_changestate}" == "true" ]; then
+    exec_ansible_playbook activate_apis.yaml
+fi
+
+## publish packages
+if [ "${apigw_data_packages_publish}" == "true" ]; then
+    exec_ansible_playbook publish_packages.yaml
+fi
+
+## publish apis
+if [ "${apigw_data_apis_publish}" == "true" ]; then
+    exec_ansible_playbook publish_apis.yaml
+fi
+
 
 logger_with_headers $LOGGER_INFO "APIGateway Configurator Done !!"
 exit 0
