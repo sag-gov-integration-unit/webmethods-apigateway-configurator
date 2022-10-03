3bae2cf2252726c925e96c9d106f552757232630
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2022-10-03 15:31:17 -0400

    adding ability to activate / deactivate apis

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -109,6 +109,10 @@ else
         exec_ansible_playbook config_apis.yaml
     fi
 
+    if [ "${data_apis_status_update,,}" == "true" ]; then
+        exec_ansible_playbook activate_apis.yaml
+    fi
+
     ## plans
     if [ "${data_plans_configure,,}" == "true" ]; then
         exec_ansible_playbook config_plans.yaml
